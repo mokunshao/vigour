e33e71360dc4b1a9a92ea0a1102a4f87249f6c5c
mokunshao <mokunshao@foxmail.com>
2019-10-21 16:00:21 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,25 @@
     <v-button icon="loading">Loading</v-button>
     <v-button icon="settings" :loading="loading" @click="loading = !loading">Settings</v-button>
     <v-button icon="left">Left</v-button>
-    <v-button icon="right">Right</v-button>
-    <v-button icon="thumbs-up">Thumbs Up</v-button>
-    <v-button icon="upload">Upload</v-button>
-    <v-button icon="download">Download</v-button>
+    <v-button icon="right" icon-position="right">Right</v-button>
+    <v-button-group>
+      <v-button icon="thumbs-up" :loading="loading" @click="loading = !loading">Thumbs Up</v-button>
+      <v-button icon="upload" :loading="loading" @click="loading = !loading">Upload</v-button>
+      <v-button icon="download" :loading="loading" @click="loading = !loading">Download</v-button>
+    </v-button-group>
     <v-button icon="loading" icon-position="right">Hello</v-button>
   </div>
 </template>
 
 <script>
 import button from './components/button.vue';
+import buttonGroup from './components/button-group.vue';
 
 export default {
   name: 'app',
   components: {
     'v-button': button,
+    'v-button-group': buttonGroup,
   },
   data() {
     return {
