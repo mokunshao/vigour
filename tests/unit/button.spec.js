dbba6bf828cd9a57679ebfcc8c8fc7f9b9e8578f
mokunshao <mokunshao@foxmail.com>
2019-10-22 01:35:09 +0800

    测试 Button

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+import Button from '@/components/button.vue';
+
+describe('button.vue', () => {
+  it('存在', () => {
+    expect(Button).to.be.ok;
+  });
+  it('可以设置 icon', () => {
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+      },
+    }).$mount();
+    const useElement = vm.$el.querySelector('use');
+    expect(useElement.getAttribute('xlink:href')).to.equal('#icon-settings');
+    vm.$destroy();
+  });
+  it('可以设置 loading', () => {
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+      propsData: {
+        icon: 'settings',
+        loading: true,
+      },
+    }).$mount();
+    const useElements = vm.$el.querySelectorAll('use');
+    expect(useElements.length).to.equal(1);
+    expect(useElements[0].getAttribute('xlink:href')).to.equal('#icon-loading');
+    vm.$destroy();
+  });
+  it('按钮显示文字', () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: 'abc',
+      },
+    });
+    expect(wrapper.text()).eq('abc');
+  });
+});
