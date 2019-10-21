7c4c4ea6a2bba0b60974a4a1d7c0e34a64931234
mokunshao <mokunshao@foxmail.com>
2019-10-21 16:01:09 +0800

    Create button-group.vue

---
+++ src/components/button-group.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="vigour-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vigour-button-group',
+};
+</script>
+
+<style lang="scss">
+.vigour-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  .vigour-button {
+    border-radius: 0;
+    margin-left: -1px;
+
+    &:first-child {
+      border-top-left-radius: 3px;
+      border-bottom-left-radius: 3px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 3px;
+      border-bottom-right-radius: 3px;
+    }
+
+    &:hover,
+    &:focus {
+      position: relative;
+    }
+  }
+}
+</style>
