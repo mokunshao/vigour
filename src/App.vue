67ec939e7383ac2f67dcf217ad2d7a262908fe33
mokunshao <mokunshao@foxmail.com>
2019-10-22 15:29:45 +0800

    添加 Input 组件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <v-button>Hello</v-button>
-    <v-button icon="loading">Loading</v-button>
-    <v-button icon="settings" :loading="loading" @click="loading = !loading">Settings</v-button>
-    <v-button icon="left">Left</v-button>
-    <v-button icon="right" icon-position="right">Right</v-button>
-    <v-button-group>
-      <v-button icon="thumbs-up" :loading="loading" @click="loading = !loading">Thumbs Up</v-button>
-      <v-button icon="upload" :loading="loading" @click="loading = !loading">Upload</v-button>
-      <v-button icon="download" :loading="loading" @click="loading = !loading">Download</v-button>
-    </v-button-group>
-    <v-button icon="loading" icon-position="right">Hello</v-button>
+    <div v-show="true">
+      <v-button>Hello</v-button>
+      <v-button icon="loading">Loading</v-button>
+      <v-button icon="settings" :loading="loading" @click="loading = !loading">Settings</v-button>
+      <v-button icon="left">Left</v-button>
+      <v-button icon="right" icon-position="right">Right</v-button>
+      <v-button-group>
+        <v-button icon="thumbs-up" :loading="loading" @click="loading = !loading"
+          >Thumbs Up</v-button
+        >
+        <v-button icon="upload" :loading="loading" @click="loading = !loading">Upload</v-button>
+        <v-button icon="download" :loading="loading" @click="loading = !loading">Download</v-button>
+      </v-button-group>
+      <v-button icon="loading" icon-position="right">Hello</v-button>
+    </div>
+    <div>
+      <v-input value="111"></v-input>
+      <v-input value="222" disabled></v-input>
+      <v-input value="333" readonly></v-input>
+      <v-input value="444" error="N/A"></v-input>
+    </div>
   </div>
 </template>
 
 <script>
 import button from './components/button.vue';
 import buttonGroup from './components/button-group.vue';
+import input from './components/input.vue';
 
 export default {
   name: 'app',
   components: {
     'v-button': button,
     'v-button-group': buttonGroup,
+    'v-input': input,
   },
   data() {
     return {
---
+++ src/components/input.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="vigour-input-wrapper" :class="{ 'vigour-input-wrapper-error': error }">
+    <input
+      type="text"
+      :value="value"
+      class="vigour-input"
+      :disabled="disabled"
+      :readonly="readonly"
+      @change="$emit('change')"
+    />
+    <template v-if="error">
+      <v-icon name="info" class="vigour-input-error-icon"></v-icon>
+      <span class="vigour-input-error-message">{{ error }}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+import icon from './icon.vue';
+
+export default {
+  name: 'vigour-input',
+  components: {
+    'v-icon': icon,
+  },
+  props: {
+    value: {
+      type: String,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    error: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import '../common.scss';
+
+.vigour-input {
+  border-radius: 3px;
+  border: 1px solid $color3;
+  padding: 0.3em 0.5em;
+  font-size: 1em;
+
+  &-wrapper {
+    display: inline-flex;
+    align-items: center;
+    vertical-align: middle;
+  }
+
+  &:hover {
+    border-color: $color2;
+  }
+
+  &:focus {
+    border-color: $color2;
+    outline: none;
+    box-shadow: inset 0 0 0 1px lighten($color: $color2, $amount: 20%);
+  }
+
+  &[disabled],
+  &:read-only {
+    cursor: not-allowed;
+    border-color: $color4;
+    color: $color4;
+    box-shadow: none;
+  }
+
+  &-error-icon {
+    fill: $dangerColor;
+  }
+
+  &-error-message {
+    color: $dangerColor;
+  }
+}
+</style>
