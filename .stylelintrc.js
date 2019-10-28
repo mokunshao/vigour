f52b05a8b432ae8953f9960e65d6991126689da5
mokunshao <mokunshao@foxmail.com>
2019-10-29 01:49:54 +0800

    取消 eslint 的 processors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  processors: [
-    [
-      '@mapbox/stylelint-processor-arbitrary-tags',
-      {
-        fileFilterRegex: [/\.vue$/],
-      },
-    ],
-  ],
+  // processors: [
+  //   [
+  //     '@mapbox/stylelint-processor-arbitrary-tags',
+  //     {
+  //       fileFilterRegex: [/\.vue$/],
+  //     },
+  //   ],
+  // ],
   extends: ['stylelint-config-standard'],
   rules: {
     'no-empty-source': null,
