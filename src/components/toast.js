d86bcae9f97eda5c821e3358a6c16157f59e076e
mokunshao <mokunshao@foxmail.com>
2019-11-04 01:06:01 +0800

    实现只显示一个 toast

--- src/components/toast.js
+++ src/components/toast.js
@@ -1,15 +1,25 @@
 import toast from './toast.vue';
 
+let toastInstance;
+
+function createToast(Vue, message, propsData) {
+  const Contructor = Vue.extend(toast);
+  const vm = new Contructor({
+    propsData,
+  });
+  vm.$slots.default = message;
+  vm.$mount();
+  document.body.appendChild(vm.$el);
+  return vm;
+}
+
 export default {
   install(Vue) {
     Vue.prototype.$toast = (message, propsData) => {
-      const Contructor = Vue.extend(toast);
-      const vm = new Contructor({
-        propsData,
-      });
-      vm.$slots.default = message;
-      vm.$mount();
-      document.body.appendChild(vm.$el);
+      if (toastInstance) {
+        toastInstance.removeNode();
+      }
+      toastInstance = createToast(Vue, message, propsData);
     };
     Vue.component(toast.name, toast);
   },
