e8f9e4a02a717d88cfb807a1b2dee3f7b846217e
mokunshao <mokunshao@foxmail.com>
2019-11-10 19:41:56 +0800

    修改 vuepress 的 webpack 配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   title: 'Vigour',
   description: 'Yet another UI Library based on Vue 2.X .',
@@ -17,5 +19,22 @@ module.exports = {
       },
     ],
     collapsable: false
-  }
+  },
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg')
+      .exclude.add(path.resolve('src/icons'))
+      .end();
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(path.resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end();
+  },
 }
