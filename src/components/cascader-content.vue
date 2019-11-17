e04b6c5f879781449a7abdbd7610b2f5d72e1727
mokunshao <mokunshao@foxmail.com>
2019-11-17 15:23:07 +0800

    完善 cascader

---
+++ src/components/cascader-content.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="vigour-cascader-content">
+    <div class="vigour-cascader-content-column">
+      <div
+        class="vigour-cascader-content-tab"
+        v-for="option in options"
+        :key="option.name"
+        @click="leftSelected = option"
+      >
+        {{ option.name }}
+      </div>
+    </div>
+    <vigour-cascader-content
+      :options="subColumn"
+      v-if="subColumn"
+    ></vigour-cascader-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vigour-cascader-content',
+  props: {
+    options: {
+      type: Array,
+    },
+  },
+  data() {
+    return {
+      leftSelected: null,
+    };
+  },
+  computed: {
+    subColumn: {
+      get() {
+        if (this.leftSelected && this.leftSelected.children) {
+          return this.leftSelected.children;
+        }
+        return null;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../common.scss";
+
+.vigour-cascader-content {
+  display: flex;
+  flex-direction: row;
+  align-items: stretch;
+
+  &-tab {
+    border: 1px solid black;
+    padding: $padding;
+  }
+}
+</style>
--- src/components/cascader.vue
+++ src/components/cascader.vue
@@ -1,38 +1,18 @@
 <template>
   <div class="vigour-cascader">
     <div class="vigour-cascader-trigger" @click="visible = !visible">input</div>
-    <div class="vigour-cascader-content" v-show="visible">
-      <div class="vigour-cascader-content-level-1">
-        <div>111</div>
-        <div>111</div>
-      </div>
-      <div class="vigour-cascader-content-level-2">
-        <div>222</div>
-        <div>222</div>
-        <div>222</div>
-      </div>
-      <div class="vigour-cascader-content-level-3">
-        <div>333</div>
-        <div>333</div>
-        <div>333</div>
-      </div>
-      <div class="temp" v-if="false">
-        <vigour-cascader-item
-          v-for="item in options"
-          :key="item.name"
-          :option="item"
-        ></vigour-cascader-item>
-      </div>
+    <div class="vigour-cascader-content-wrapper" v-show="visible">
+      <vigour-cascader-content :options="options"></vigour-cascader-content>
     </div>
   </div>
 </template>
 
 <script>
-import cascaderItem from './cascader-item.vue';
+import cascaderContent from './cascader-content.vue';
 
 export default {
   name: 'vigour-cascader',
-  components: { [cascaderItem.name]: cascaderItem },
+  components: { [cascaderContent.name]: cascaderContent },
   props: {
     options: {
       type: Array,
@@ -48,26 +28,16 @@ export default {
 
 <style lang="scss" scoped>
 .vigour-cascader {
-  color: black;
   position: relative;
 
-  &-content,
+  &-content-wrapper,
   &-trigger {
     border: 1px solid black;
   }
 
-  &-content {
+  &-content-wrapper {
     position: absolute;
-    display: flex;
-    flex-direction: row;
-    align-items: stretch;
     background-color: white;
-
-    &-level-1,
-    &-level-2,
-    &-level-3 {
-      border: 1px solid black;
-    }
   }
 }
 </style>
