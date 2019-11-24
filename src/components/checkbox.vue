8cc72260feacdab62cf3fefb568cd4b9f3467bb2
mokunshao <mokunshao@foxmail.com>
2019-11-24 22:02:54 +0800

    修改 checkbox 样式

--- src/components/checkbox.vue
+++ src/components/checkbox.vue
@@ -29,15 +29,49 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../common.scss";
+
 .vigour-checkbox {
-  border: 1px solid black;
   display: inline-flex;
   align-items: center;
+  cursor: pointer;
 
-  // &-input {
-  //   opacity: 0;
-  //   width: 0;
-  //   height: 0;
-  // }
+  &-input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+    position: absolute;
+  }
+
+  &-checkmark {
+    display: inline-flex;
+    height: 1em;
+    width: 1em;
+    margin-right: 0.2em;
+    margin-left: 0.2em;
+    border: 2px solid $black;
+    transition: background-color 200ms;
+  }
+
+  &:hover &-checkmark {
+    background-color: $grey;
+  }
+
+  &-input:checked ~ &-checkmark {
+    background-color: $black;
+    position: relative;
+
+    &::after {
+      content: "";
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      width: 0.4em;
+      height: 0.7em;
+      border: solid white;
+      border-width: 0 0.1em 0.1em 0;
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
 }
 </style>
