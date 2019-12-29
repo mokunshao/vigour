eb73a9a71c17df94ac04e28385a37d9a045d7591
mokunshao <mokunshao@foxmail.com>
2019-12-29 22:03:36 +0800

    Create tree.spec.js

---
+++ src/components/tree/__tests__/tree.spec.js
@@ -0,0 +1,9 @@
+import Tree from '../tree.vue';
+import TreeItem from '../tree-item.vue';
+
+describe('Tree', () => {
+  it('存在', () => {
+    expect(Tree).toBeTruthy();
+    expect(TreeItem).toBeTruthy();
+  });
+});
