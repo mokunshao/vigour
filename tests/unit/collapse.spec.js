52a15011c414e9097fbc880bc64adbf5b628edd2
mokunshao <mokunshao@foxmail.com>
2020-01-12 23:17:25 +0800

    更新 collapse 的测试

--- tests/unit/collapse.spec.js
+++ tests/unit/collapse.spec.js
@@ -1,4 +1,3 @@
-import { expect } from 'chai';
 import { mount } from '@vue/test-utils';
 import Vue from 'vue';
 import Collapse from '@/components/collapse.vue';
@@ -7,11 +6,11 @@ import CollapseItem from '@/components/collapse-item.vue';
 Vue.component(CollapseItem.name, CollapseItem);
 describe('collapse.vue', () => {
   it('存在', () => {
-    expect(Collapse).to.be.ok;
-    expect(CollapseItem).to.be.ok;
+    expect(Collapse).toBeTruthy();
+    expect(CollapseItem).toBeTruthy();
   });
   it('接受 selected 属性', () => {
-    const unfolds = ['title 1', 'title 2', 'title 3'];
+    const unfolds = ['title 1', 'title 2'];
     const wrapper = mount(Collapse, {
       propsData: {
         value: unfolds,
@@ -22,9 +21,9 @@ describe('collapse.vue', () => {
         <vigour-collapse-item id='t3' title="title 3">content 3</vigour-collapse-item>`,
       },
     });
-    expect(wrapper.find('#t1 .vigour-collapse-item-content').isVisible()).to.be.true;
-    expect(wrapper.find('#t2 .vigour-collapse-item-content').isVisible()).to.be.true;
-    expect(wrapper.find('#t3 .vigour-collapse-item-content').isVisible()).to.be.true;
+    expect(wrapper.find('#t1 .vigour-collapse-item-content').isVisible()).toBeTruthy();
+    expect(wrapper.find('#t2 .vigour-collapse-item-content').isVisible()).toBeTruthy();
+    expect(wrapper.find('#t3 .vigour-collapse-item-content').exists()).toBeFalsy();
   });
   it('接受 single 属性', () => {
     const unfolds = ['title 2'];
@@ -34,11 +33,11 @@ describe('collapse.vue', () => {
         single: true,
       },
       slots: {
-        default: `<vigour-collapse-item id='t1' title="title 1">content 1</vigour-collapse-item>
-        <vigour-collapse-item id='t2' title="title 2">content 2</vigour-collapse-item>`,
+        default: `<vigour-collapse-item id='t4' title="title 1">content 1</vigour-collapse-item>
+        <vigour-collapse-item id='t5' title="title 2">content 2</vigour-collapse-item>`,
       },
     });
-    expect(wrapper.find('#t1 .vigour-collapse-item-content').isVisible()).to.be.false;
-    expect(wrapper.find('#t2 .vigour-collapse-item-content').isVisible()).to.be.true;
+    expect(wrapper.find('#t4 .vigour-collapse-item-content').exists()).toBeFalsy();
+    expect(wrapper.find('#t5 .vigour-collapse-item-content').isVisible()).toBeTruthy();
   });
 });
